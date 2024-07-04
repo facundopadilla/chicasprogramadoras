b0da995c36d865cbc015e140f5c82f6b00fbb058
Facundo Padilla <64610246+facundopadilla@users.noreply.github.com>
2024-07-04 18:43:43 -0300

    codigo para sumar dos numeros

---
+++ script.js
@@ -0,0 +1,6 @@
+function sumar() {
+	let num1 = parseFloat(document.getElementById('num1').value)
+	let num2 = parseFloat(document.getElementById('num2').value)
+    let resultado = num1 + num2
+    document.getElementById('resultado').textContent = resultado;
+}
